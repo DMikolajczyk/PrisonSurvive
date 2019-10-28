70c95e7f5701268f4ee8d1dadcd7479f9a51b83d
DMikolajczyk <dam.mikolajczyk@gmail.com>
2019-10-28 20:08:18 +0100

    Add finding cell by prisoner
    Basics of finding own cell - by press O, prisoners go to front of their cells

--- PrisonSurvive/Assets/Scripts/Character/PrisonerEnemy.cs
+++ PrisonSurvive/Assets/Scripts/Character/PrisonerEnemy.cs
@@ -11,11 +11,17 @@ public class PrisonerEnemy : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 2.0f,
                 rotateSpeed = 5.0f,
-                distBlocker = 5.0f;
+                distBlocker = 5.0f,
+                distThreshold = 0.2f;
+    [SerializeField]
+    private int cell_id = 1;
 
 
 
     private bool setTargetForPlayer = false;
+    private bool setTargetForCell = false;
+    private bool isWaitingForMove = false;
+    private bool isMoving = false;
     private Transform player; 
     private Vector3 target;
     private Vector3 playerPositionCorrected;
@@ -33,40 +39,81 @@ public class PrisonerEnemy : MonoBehaviour
     private void Update()
     {
         Animation();
-        if(Input.GetKeyUp(KeyCode.P))
+        if (!isWaitingForMove)
         {
-            setTargetForPlayer = !setTargetForPlayer;
+            if (Input.GetKeyUp(KeyCode.P))
+            {
+                setTargetForPlayer = !setTargetForPlayer;
+                if (setTargetForPlayer)
+                {
+                    target = new Vector3(player.position.x, transform.position.y, player.position.z);
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.O))
+            {
+                setTargetForCell = !setTargetForCell;
+                if (setTargetForCell)
+                {
+                    target = new Vector3(findCell(0, cell_id).x, transform.position.y, findCell(0, cell_id).z);
+                }
+            }
             if (setTargetForPlayer)
             {
-                target = new Vector3(player.position.x, transform.position.y, player.position.z);
+                goToTarget(target, true, distBlocker);
             }
-        }
-        if (setTargetForPlayer)
-        {
-            goToTarget(target, true);
-            //actualMoveSpeed = moveSpeed;
-        }
-        else
-        {
-            actualMoveSpeed = 0.0f;
+            else if (setTargetForCell)
+            {
+                goToTarget(target, false, distThreshold);
+            }
+            else
+            {
+                actualMoveSpeed = 0.0f;
+                isMoving = false;
+            }
         }
     }
 
-    private void goToTarget(Vector3 pos, bool targetCanMove)
+    private Vector3 findCell (int floor, int cell_id)
+    {
+        string floorString = convertNumberToString(floor);
+        string cell_idString = convertNumberToString(cell_id);
+        GameObject cellPref = GameObject.Find("Cell_" + floorString + "_" + cell_idString);
+        Transform frontOfCell = cellPref.transform.Find("PositionPoint_Front");
+        Transform centerOfCell = cellPref.transform.Find("PositionPoint_Center");
+        return frontOfCell.position;
+    }
+
+    private string convertNumberToString(int num)
+    {
+        if (num < 10)
+        {
+            return  ("0" + num);
+        }
+        else
+        {
+            return num.ToString();
+        }
+    }
+
+    private void goToTarget(Vector3 pos, bool targetCanMove, float distanceToBlock)
     {
         if (targetCanMove)
         {
             target = new Vector3(player.position.x, transform.position.y, player.position.z);
         }
+
         distToTarget = Vector3.Distance(transform.position, target);
-        if (distToTarget > distBlocker)
+        if (distToTarget < distanceToBlock)
         {
-            actualMoveSpeed = moveSpeed;
+            actualMoveSpeed = 0.0f;
+            isMoving = false;
         }
         else
         {
-            actualMoveSpeed = 0.0f;
+            actualMoveSpeed = moveSpeed;
+            isMoving = true;
         }
+                
         RotateToTarget(target);
     }
 
@@ -81,6 +128,33 @@ public class PrisonerEnemy : MonoBehaviour
         if(animator != null)
         {
             animator.SetFloat("speed", actualMoveSpeed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PrisonerEnemy otherPrisoner = other.GetComponent<PrisonerEnemy>();
+        if (otherPrisoner != null)
+        {
+            SolvePrisonersCollision(otherPrisoner);
+        }
+    }
+
+    private void SolvePrisonersCollision(PrisonerEnemy p)
+    {
+        if((p.cell_id > cell_id) && p.isMoving)
+        {
+            isWaitingForMove = true;
+            actualMoveSpeed = 0.0f;
+            isMoving = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PrisonerEnemy otherPrisoner = other.GetComponent<PrisonerEnemy>();
+        if (otherPrisoner != null)
+        {
+            isWaitingForMove = false;
         }
     }
 }
